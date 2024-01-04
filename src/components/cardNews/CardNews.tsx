4da4bcb220728fc3adb46b1c7ccc5b567af09b16
victor-romero-martinez <victordav147@gmail.com>
2024-01-04 12:23:23 -0300

    {update} card product and news section responsive

--- src/components/cardNews/CardNews.tsx
+++ src/components/cardNews/CardNews.tsx
@@ -12,11 +12,13 @@ export default function CardNews({ imgUrl, date, title, description }: {
       <picture>
         <Image src={imgUrl} width={320} height={240} alt={`picture of ${imgUrl}`} />
       </picture>
-      <span>{date}</span>
-      <a href="#">
-        <h4>{title}</h4>
-      </a>
-      <p>{description}</p>
+      <div>
+        <span>{date}</span>
+        <a href="#">
+          <h4>{title}</h4>
+        </a>
+        <p>{description}</p>
+      </div>
     </article>
   )
 };
