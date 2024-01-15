7a6a1e4a6534b3c788eed5a6139b9099086adbe5
victor-romero-martinez <victordav147@gmail.com>
2024-01-15 17:58:02 -0300

    [add] cart emty icon

--- src/components/cartView/CartView.tsx
+++ src/components/cartView/CartView.tsx
@@ -1,41 +1,54 @@
 /* eslint-disable @next/next/no-img-element */
 import { Product } from '@/definitions/type'
-import { DollarIcon, TrashIcon } from '../icons/icons'
+import { BagEmptyIcon, DollarIcon, TrashIcon } from '../icons/icons'
 import { useUIShopStore } from '@/store/shopStore'
 
 import './style.css'
 
 
 export default function CartView({ items }: { items: Product[] }) {
-  const remove = useUIShopStore(state => state.removeToCart)
+  const { shopingCart } = useUIShopStore()
+  const { removeToCart } = useUIShopStore()
 
-  const handleRemove = (id: string | number) => remove(id)
+  const handleRemove = (id: string | number) => removeToCart(id)
 
   return (
-    <>
-      {items?.map(i => (
-        <div key={i.id} className='cart__container__item w-full'>
-          <picture className='padding'>
-            <img src={i.images[0]} alt={i.title} width={50} />
-          </picture>
-          <div className='cart__container__item-inner w-full'>
-            <div className='cart__container__item-details'>
-              <h4>{i.title}</h4>
-              <p>
-                Price: &nbsp; <span> {i.price - i.discountPercentage}</span>
-                <DollarIcon width='14' height='14' />
-              </p>
+    <>{
+      shopingCart.length > 0 ? (
+        <>
+          {items?.map(i => (
+            <div key={i.id} className='cart__container__item w-full'>
+              <picture className='padding'>
+                <img src={i.images[0]} alt={i.title} width={50} />
+              </picture>
+              <div className='cart__container__item-inner w-full'>
+                <div className='cart__container__item-details'>
+                  <h4>{i.title}</h4>
+                  <p>
+                    Price: &nbsp; <span> {i.price - i.discountPercentage}</span>
+                    <DollarIcon width='14' height='14' />
+                  </p>
+                </div>
+                <button
+                  type='button'
+                  title='Delete item'
+                  onClick={() => handleRemove(i.id)}
+                >
+                  <TrashIcon />
+                </button>
+              </div>
             </div>
-            <button
-              type='button'
-              title='Delete item'
-              onClick={() => handleRemove(i.id)}
-            >
-              <TrashIcon />
-            </button>
-          </div>
+          ))}
+        </>
+      ) : (
+        <div className='cart__container__item-empty w-full'>
+          <span>
+            <BagEmptyIcon width='60' height='60' />
+            Cart is empty :(
+          </span>
         </div>
-      ))}
+      )
+    }
     </>
   )
 };
