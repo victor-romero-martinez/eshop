08d12811b2ca5deabbf34894c994721be86b1b65
Victo Romero <104655407+victor-romero-martinez@users.noreply.github.com>
2024-01-18 15:01:19 -0300

    Actualizar formatedString.ts
    [refractor]

--- src/lib/formatedString.ts
+++ src/lib/formatedString.ts
@@ -1,6 +1,6 @@
 /** Fix string */
-export function formatedString(arr: string = "") {
-  let res = arr.split("-").join(" ");
+export function formatedString(arr: string) {
+  let res = arr.replace("-", " ");
   res = res.charAt(0).toUpperCase() + res.slice(1);
 
   return res;
