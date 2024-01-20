32c6b95901c6f059d4ac2fb5fca5a3b843a30afc
victor-romero-martinez <victordav147@gmail.com>
2024-01-20 16:59:32 -0300

    [fix] typo

--- src/lib/fetch/postFetch.ts
+++ src/lib/fetch/postFetch.ts
@@ -1,14 +1,17 @@
-/** Post data */
+/** Post data
+ * @param url
+ * @param body
+ */
 export async function postData<T>(url: string, body: object): Promise<T> {
   const res = await fetch(url, {
     method: "POST",
-    headers: { "Content-Type": "application/jsno" },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
     cache: "no-store",
   });
 
   if (!res.ok) {
-    console.error("Filed to fetching data", res.status);
+    console.error("Filed to fetching data. Status:", res.status);
   }
 
   return await (res.json() as Promise<T>);
